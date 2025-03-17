79d17ea573f3b517164be45351fb860f651287cc
StormiHUN <stormihun@gmail.com>
2025-03-17 14:46:19 +0100

    Beggining of cart system

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import Allergens from './Allergens'
+import Cart from "../assets/cart.svg"
+import CartGreen from "../assets/cartgreen.svg"
+import { useState } from 'react'
 const Food = ({ data }) => {
+
+    const [onhover, setonhover] = useState(false)
+
     return (
         <div className='bg-white border-6 border-[#93e2ae] rounded-lg w-fit'>
             <img src={data.pic} />
@@ -12,6 +18,9 @@ const Food = ({ data }) => {
                 <div className='my-auto'>
                     <p className='text-xl'>{data.cost},- Ft</p>
                 </div>
+            </div>
+            <div className='p-1'>
+                <button className='flex gap-2 p-1 border-2 border-white hover:border-[#93e2ae] transition-all rounded-full' onMouseOver={() => setonhover(true)}  onMouseEnter={() => setonhover(true)} onMouseLeave={() => setonhover(false)}><img className='p-1' src={onhover ? CartGreen : Cart} /></button>
             </div>
         </div>
     )
